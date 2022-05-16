b11ebdc0673a3428a61f44f2769f439694b6dfa8
Philipp Perez <philippperezhubert@gmail.com>
2022-05-16 10:48:53 -0400

    Neues Projekt erstellt

---
+++ csn_phonebook/csn_phonebook.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	int input = 0;
+	for (;;) {
+
+		const char test[10] = { 'A', 'B', 'C', 0, 'F' };
+		const char* test2 = "ABC";
+		printf("Hello World: %s\n\t", test2);
+
+	
+		//printf("Bitte Zahl eingeben: ");
+		//scanf_s("%d", &input);
+
+		//printf("Diese Zahl wurde eingegeben: %d\n", input);
+	}
+
+	return 0;
+}
