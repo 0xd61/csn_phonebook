3d85624cd337b23fd0096a0beed360e7dd67d836
Felisnepe <philippperezhubert@gmail.com>
2022-05-16 20:18:35 -0400

    Update 16/05

--- csn_phonebook/csn_phonebook.cpp
+++ csn_phonebook/csn_phonebook.cpp
@@ -1,20 +1,32 @@
-#include <stdio.h>
+#include<stdio.h> 
+#define size 1 
 
-int main() {
-	// printf() displays the string inside quotation
-	int input = 0;
-	for (;;) {
+int main()
+{
+    struct Daten
+    {
+        char Name[30];
+        int Age[3];
+        int PhoneNumber[100];
+        char Gender[10];
+        char EMail[100];
+    };
+    struct Daten Daten1 = { "Philipp Perez", 18 };
+    printf("Daten1 = %s\n", Daten1.Name);
+    printf("Daten1 = %f\n", Daten1.Age);
 
-		const char test[10] = { 'A', 'B', 'C', 0, 'F' };
-		const char* test2 = "ABC";
-		printf("Hello World: %s\n\t", test2);
+    int index = 0;
 
-	
-		//printf("Bitte Zahl eingeben: ");
-		//scanf_s("%d", &input);
+    for (index = 0; index < size; index++)
+        printf("\t\t\t\t\t============PHONEBOOK============");
+    printf("\n\t\t 1) Name");
+    printf("\n\t\t 2) Age");
+    printf("\n\t\t 3) Phone-Number");
+    printf("\n\t\t 4) Gender");
+    printf("\n\t\t 5) E-Mail");
 
-		//printf("Diese Zahl wurde eingegeben: %d\n", input);
-	}
 
-	return 0;
+
+    return 0;
 }
+
